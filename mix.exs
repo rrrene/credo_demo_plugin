8cb5854de9de2f07144dcde5afda5f136cc49db8
René Föhring <rene.foehring@5minds.de>
2019-06-06 06:32:56 +0200

    Update license and add license to mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,26 @@ defmodule CredoDemoPlugin.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A static code analysis tool with a focus on code consistency and teaching.",
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      files: [
+        ".credo.exs",
+        "lib",
+        "mix.exs",
+        "README.md",
+        "LICENSE"
+      ],
+      maintainers: ["René Föhring"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/rrrene/credo_demo_plugin"
+      }
     ]
   end
 
