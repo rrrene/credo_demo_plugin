353fc6ac003f5a4f9765d8849615319f57a43e43
René Föhring <rf@bamaru.de>
2020-02-15 08:33:50 +0100

    Bump version to v0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CredoDemoPlugin.MixProject do
   def project do
     [
       app: :credo_demo_plugin,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
